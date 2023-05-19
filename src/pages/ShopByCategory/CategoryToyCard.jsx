9e1f5dfd94c81674ccce9dfdbd8be6741707a1bb
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-19 17:58:11 +0600

    shop by category added

---
+++ src/pages/ShopByCategory/CategoryToyCard.jsx
@@ -0,0 +1,44 @@
+import { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
+
+const CategoryToyCard = ({ subCategoryToy }) => {
+    console.log(subCategoryToy);
+
+    const { _id, name, productName, price, quantity, subCategory, toyPhoto } = subCategoryToy;
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const { user } = useContext(AuthContext);
+
+    return (
+        <>
+            <div
+                className="bg-white rounded-lg overflow-hidden shadow-md relative"
+                onMouseEnter={() => setIsHovered(true)}
+                onMouseLeave={() => setIsHovered(false)}
+            >
+                <img
+                    className="w-full h-48 object-cover"
+                    src={toyPhoto}
+                    alt="Toy Image"
+                />
+                <div className="p-4">
+                    <h3 className="font-bold text-black-800 mb-2">{productName}</h3>
+                    <p className="text-gray-600 text-sm mb-2">Sub-category: <span className="text-[#ab6032f1]">{subCategory}</span></p>
+                    <p className="text-gray-600 text-sm mb-2">Price: $ <span className="text-[#ab6032f1]">{price}</span></p>
+                    <p className="text-gray-600 text-sm mb-2">Posted by: <span className="text-[#ab6032f1]">{name}</span></p>
+                    {isHovered && (
+                        <div className="absolute bottom-0 left-0 w-full bg-white bg-opacity-90 p-4 flex justify-between items-center">
+                            <Link to={user?.email ? `/details/${_id}` : '/sign-in'} className="bg-[#ab6032f1] hover:bg-[#944e22f1] text-white py-2 px-4 rounded-md">View Details</Link>
+                            <p className="text-gray-600 text-sm mb-0">Available quantity: <span className="text-[#ab6032f1]">{quantity}</span></p>
+                        </div>
+                    )}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default CategoryToyCard;
--- src/pages/ShopByCategory/ShopByCategory.jsx
+++ src/pages/ShopByCategory/ShopByCategory.jsx
@@ -1,24 +1,39 @@
 
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
-import './ShopByCategory.css';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { useEffect, useState } from 'react';
+import CategoryToyCard from './CategoryToyCard';
 
 const ShopByCategory = () => {
 
-        const autoSLider = {
-          dots: true,
-          infinite: true,
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          autoplay: true,
-          speed: 2000,
-          autoplaySpeed: 2000,
-          cssEase: "linear"
-        };
-    
+    const autoSLider = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        speed: 2000,
+        autoplaySpeed: 2000,
+        cssEase: "linear"
+    };
+
+    const category = ['Regular', 'Police', 'Jeep', 'Sports'];
+    const [tabIndex, setTabIndex] = useState(0);
+    const [subCategory, setSubCategory] = useState('');
+    const [subCategoryToys, setSubCategoryToys] = useState([]);
+    // console.log(subCategory);
+
+    useEffect(() => {
+        fetch(`https://toy-crate-x-server.vercel.app/toy/${subCategory}`)
+            .then(res => res.json())
+            .then(data => {
+                // console.log(data);
+                setSubCategoryToys(data);
+            })
+    }, [subCategory])
 
 
     return (
@@ -28,49 +43,88 @@ const ShopByCategory = () => {
             </h1>
 
             <div>
-        <Slider {...autoSLider} className='py-12'>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/9yyFJHx/2.jpg" alt="" />
-          </div>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/PFy3YWy/4.jpg" alt="" />
-          </div>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/wK1Jt4f/7.jpg" alt="" />
-          </div>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/yks0PHW/6.jpg" alt="" />
-          </div>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/k8h5QHn/3.jpg" alt="" />
-          </div>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/1Z9tDrC/1.jpg" alt="" />
-          </div>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/zxDw9D5/1.jpg" alt="" />
-          </div>
-          <div>
-            <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/RTgpk0Z/3.jpg" alt="" />
-          </div>
-        </Slider>
-      </div>
+                <Slider {...autoSLider} className='py-12'>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/9yyFJHx/2.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/PFy3YWy/4.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/wK1Jt4f/7.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/yks0PHW/6.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/k8h5QHn/3.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/1Z9tDrC/1.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/zxDw9D5/1.jpg" alt="" />
+                    </div>
+                    <div>
+                        <img className='h-52 w-60 rounded-lg shadow-md hover:opacity-50' src="https://i.ibb.co/RTgpk0Z/3.jpg" alt="" />
+                    </div>
+                </Slider>
+            </div>
 
+            <div className="py-12">
+                <Tabs selectedIndex={tabIndex} onSelect={(index) => {
+                    setTabIndex(index);
+                    setSubCategory(category[index]);
+                }}>
+                    <TabList>
+                        <Tab><span className='font-bold text-[#ab6032f1]'>Regular Car</span></Tab>
+                        <Tab><span className='font-bold text-[#ab6032f1]'>Police Car</span></Tab>
+                        <Tab><span className='font-bold text-[#ab6032f1]'>Jeep</span></Tab>
+                        <Tab><span className='font-bold text-[#ab6032f1]'>Sports Car</span></Tab>
+                    </TabList>
 
-
-            <Tabs>
-                <TabList>
-                    <Tab>Title 1</Tab>
-                    <Tab>Title 2</Tab>
-                </TabList>
-
-                <TabPanel>
-                    <h2>Any content 1</h2>
-                </TabPanel>
-                <TabPanel>
-                    <h2>Any content 2</h2>
-                </TabPanel>
-            </Tabs>
+                    <TabPanel>
+                        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 bg-[#e2d5d566]">
+                            {
+                                subCategoryToys.map(subCategoryToy => <CategoryToyCard
+                                    key={subCategoryToy._id}
+                                    subCategoryToy={subCategoryToy}
+                                ></CategoryToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 bg-[#e2d5d566]">
+                            {
+                                subCategoryToys.map(subCategoryToy => <CategoryToyCard
+                                    key={subCategoryToy._id}
+                                    subCategoryToy={subCategoryToy}
+                                ></CategoryToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 bg-[#e2d5d566]">
+                            {
+                                subCategoryToys.map(subCategoryToy => <CategoryToyCard
+                                    key={subCategoryToy._id}
+                                    subCategoryToy={subCategoryToy}
+                                ></CategoryToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div className="mt-3 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-12 bg-[#e2d5d566]">
+                            {
+                                subCategoryToys.map(subCategoryToy => <CategoryToyCard
+                                    key={subCategoryToy._id}
+                                    subCategoryToy={subCategoryToy}
+                                ></CategoryToyCard>)
+                            }
+                        </div>
+                    </TabPanel>
+                </Tabs>
+            </div>
         </div>
     );
 };
